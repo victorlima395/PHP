17db92fbc03eae902cb24b5b6a0c319b402af5ca
victorlima395 <victorlima395@gmail.com>
2019-05-03 22:00:09 -0300

    php
    php

---
+++ aula02/index.php
@@ -0,0 +1,40 @@
+<?php 
+
+$nome = "Victor Lima";
+$idade = 19;
+
+echo "Nome Completo: $nome</br>Idade: $idade";
+
+$soma = 20 + 20;
+$subtracao = 20 - 20;
+$divisao = 20 / 20;
+$multiplicacao = 20 * 20;
+
+echo "<br>";
+
+echo "Soma: ".$soma;
+
+echo "<br>";
+
+$incremento = 1;
+echo ++$incremento;
+
+echo "<br>";
+
+
+// OPERADORES RELACIONAIS
+// igual: ==
+// diferente != ou <>
+// menor: <
+// maior >
+// menor ou igual: <=
+// maior ou igual >=
+
+// OPERADORES LÓGICOS
+// e: && ou and
+// ou || ou or
+// xor
+// negação: !
+
+
+?>
